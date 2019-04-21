451ec380a820b7037c1154daed686d6ffa3ab683
王玲峰 <wanglingfeng@wanglingfengdeMacBook-Pro.local>
2019-04-21 15:32:41 +0800

    1.添加collectionView
    1.collectionView

---
+++ 6.UITableView和UICollectionView/Collectionview/CollectionView/CollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by 王玲峰 on 4/21/19.
+//  Copyright © 2019 王玲峰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
